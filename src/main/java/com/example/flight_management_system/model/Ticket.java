36846fbebcfee18fa945d05cc001f6589d6d764d
biancaholobica <bianca.ioana.holobica@stud.ubbcluj.ro>
2025-10-26 14:25:28 +0200

    model ticket

--- src/main/java/com/example/flight_management_system/model/Ticket.java
+++ src/main/java/com/example/flight_management_system/model/Ticket.java
@@ -9,4 +9,60 @@ public class Ticket {
     private double price;
     private String seatNumber;
     private List<Luggage> luggages;
+
+    public Ticket(String id, String passengerId, String flightId, double price, String seatNumber) {
+        this.id = id;
+        this.passengerId = passengerId;
+        this.flightId = flightId;
+        this.price = price;
+        this.seatNumber = seatNumber;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String setId(){
+        return id;
+    }
+
+    public String getPassengerId() {
+        return passengerId;
+    }
+
+    public void setPassengerId(String passengerId) {
+        this.passengerId = passengerId;
+    }
+
+    public String getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(String flightId) {
+        this.flightId = flightId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public List<Luggage> getLuggages() {
+        return luggages;
+    }
+
+    public void setLuggages(List<Luggage> luggages) {
+        this.luggages = luggages;
+    }
 }
