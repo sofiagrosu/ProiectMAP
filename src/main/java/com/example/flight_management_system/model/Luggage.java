01120bcd8d07f056627e2cc37b5a5a3d7f1c1f65
biancaholobica <bianca.ioana.holobica@stud.ubbcluj.ro>
2025-10-26 14:27:11 +0200

    model luggage

--- src/main/java/com/example/flight_management_system/model/Luggage.java
+++ src/main/java/com/example/flight_management_system/model/Luggage.java
@@ -4,4 +4,34 @@ public class Luggage {
     private String id;
     private String ticketId;
     private String status;
+
+    public Luggage(String id, String ticketId, String status) {
+        this.id = id;
+        this.ticketId = ticketId;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
