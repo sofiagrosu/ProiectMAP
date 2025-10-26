6051c0b27511360ae7969f0dfba5f9e7cd9e39cd
biancaholobica <bianca.ioana.holobica@stud.ubbcluj.ro>
2025-10-26 15:04:40 +0200

    PassengerRepository

--- src/main/java/com/example/flight_management_system/repository/PassengerRepository.java
+++ src/main/java/com/example/flight_management_system/repository/PassengerRepository.java
@@ -1,4 +1,42 @@
 package com.example.flight_management_system.repository;
 
+import com.example.flight_management_system.model.Passenger;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PassengerRepository {
+    private List<Passenger> passengers =  new ArrayList<>();
+    private long nextId = 1;
+
+    public Passenger save(Passenger passenger) {
+        if (passenger.getId() == null) {
+            passenger.setId(String.valueOf(nextId));
+            nextId++;
+        }
+        passengers.add(passenger);
+        return passenger;
+    }
+
+    public List<Passenger> findAll() {
+        return new ArrayList<>(passengers);
+    }
+
+    public Passenger findById(String id) {
+        for (Passenger passenger : passengers) {
+            if (passenger.getId().equals(id)) {
+                return passenger;
+            }
+        }
+        return null;
+    }
+
+    public boolean delete(String id){
+        Passenger passenger = findById(id);
+        if(passenger != null){
+            passengers.remove(passenger);
+            return true;
+        }
+        return false;
+    }
 }
