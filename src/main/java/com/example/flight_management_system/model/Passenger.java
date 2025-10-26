3aa889113f4d36922188d3cd99d3b57158aa9fd6
biancaholobica <bianca.ioana.holobica@stud.ubbcluj.ro>
2025-10-26 14:22:07 +0200

    model passenger

--- src/main/java/com/example/flight_management_system/model/Passenger.java
+++ src/main/java/com/example/flight_management_system/model/Passenger.java
@@ -8,4 +8,41 @@ public class Passenger {
     private String currency;
     private List<Ticket> tickets;
 
+    public Passenger(String id, String name, String currency, List<Ticket> tickets) {
+        this.id = id;
+        this.name = name;
+        this.currency = currency;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
 }
