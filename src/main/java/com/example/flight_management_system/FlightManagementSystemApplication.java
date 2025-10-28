671332cace4d3e6f0c726c6d95f50b470a5d898b
biancaholobica <bianca.ioana.holobica@stud.ubbcluj.ro>
2025-10-28 12:52:01 +0200

    afisare date

--- src/main/java/com/example/flight_management_system/FlightManagementSystemApplication.java
+++ src/main/java/com/example/flight_management_system/FlightManagementSystemApplication.java
@@ -2,8 +2,11 @@ package com.example.flight_management_system;
 
 import com.example.flight_management_system.model.Flight;
 import com.example.flight_management_system.repository.FlightRepository;
+import com.example.flight_management_system.service.FlightService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FlightManagementSystemApplication {
@@ -12,7 +15,19 @@ public class FlightManagementSystemApplication {
 
         SpringApplication.run(FlightManagementSystemApplication.class, args);
 
-
 	}
+
+    @Bean
+    public CommandLineRunner initFlights(FlightService flightService) {
+        return args -> {
+            Flight flight1 = new Flight(null, "Flight 101", "NB1", "AP1", "G1");
+            Flight flight2 = new Flight(null, "Flight 202", "NB2", "AP2", "G2");
+            Flight flight3 = new Flight(null, "Flight 303", "NB3", "AP3", "G3");
+
+            flightService.save(flight1);
+            flightService.save(flight2);
+            flightService.save(flight3);
+        };
+    }
 
 }
