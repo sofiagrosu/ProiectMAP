f214b50a73e53b98f5dd1f330df804e7cad99d6c
sofiagrosu <grosu.sofia@gmail.com>
2025-12-12 20:18:22 +0200

    sort passengers

--- src/main/java/com/example/flight_management_system/controller/PassengerController.java
+++ src/main/java/com/example/flight_management_system/controller/PassengerController.java
@@ -1,9 +1,15 @@
 package com.example.flight_management_system.controller;
 
+import com.example.flight_management_system.model.Luggage;
 import com.example.flight_management_system.model.Passenger;
 import com.example.flight_management_system.repository.PassengerRepository;
+import com.example.flight_management_system.service.PassengerService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,12 +20,40 @@ import org.springframework.web.bind.annotation.*;
 public class PassengerController {
 
     @Autowired
-    private PassengerRepository passengerRepository;
+    private PassengerService passengerService;
 
     @GetMapping
-    public String listPassengers(Model model) {
-        model.addAttribute("passengers", passengerRepository.findAll());
+    public String listPassengers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "true") boolean ascending,
+            Model model)
+    {
+
+        Sort sort = buildSort(sortBy, ascending);
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<Passenger> objectPage = passengerService.findAll(pageable);
+        model.addAttribute("passengers", objectPage.getContent());     // pentru tabel
+        model.addAttribute("page", objectPage);                     // pentru paginare (opțional)
+        model.addAttribute("sortBy", sortBy);
+        model.addAttribute("ascending", ascending);
+
         return "passengers/index";
+
+
+    }
+    private Sort buildSort(String sortBy, boolean ascending) {
+        Sort.Direction dir = ascending ? Sort.Direction.ASC : Sort.Direction.DESC;
+
+        return switch (sortBy) {
+            case "id" -> Sort.by(dir, "id");
+            case "name" -> Sort.by(dir, "name");
+            case "currency" -> Sort.by(dir, "currency");
+            case "isCheckedIn" -> Sort.by(dir, "isCheckedIn");
+
+            default -> Sort.by(Sort.Direction.ASC, "id");
+        };
     }
 
     @GetMapping("/new")
@@ -34,30 +68,30 @@ public class PassengerController {
         if (result.hasErrors()) return "passengers/form";
 
 
-        passengerRepository.save(passenger);
+        passengerService.save(passenger);
         return "redirect:/passengers";
     }
 
     @GetMapping("/edit/{id}")
     public String editPassengerForm(@PathVariable("id") Long id, Model model) {
-        Passenger passenger = passengerRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid passenger Id:" + id));
+        Passenger passenger = passengerService.findById(id);
+               // .orElseThrow(() -> new IllegalArgumentException("Invalid passenger Id:" + id));
         model.addAttribute("passenger", passenger);
         return "passengers/form";
     }
 
     @GetMapping("/delete/{id}")
     public String deletePassenger(@PathVariable("id") Long id) {
-        Passenger passenger = passengerRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid passenger Id:" + id));
-        passengerRepository.delete(passenger);
+        Passenger passenger = passengerService.findById(id);
+               // .orElseThrow(() -> new IllegalArgumentException("Invalid passenger Id:" + id));
+        passengerService.delete(passenger.getId());
         return "redirect:/passengers";
     }
 
     @GetMapping("/details/{id}")
     public String passengerDetails(@PathVariable("id") Long id, Model model) {
-        Passenger passenger = passengerRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid passenger Id:" + id));
+        Passenger passenger = passengerService.findById(id);
+              //  .orElseThrow(() -> new IllegalArgumentException("Invalid passenger Id:" + id));
         model.addAttribute("passenger", passenger);
         return "passengers/details";
     }
--- src/main/java/com/example/flight_management_system/service/PassengerService.java
+++ src/main/java/com/example/flight_management_system/service/PassengerService.java
@@ -5,12 +5,13 @@ import com.example.flight_management_system.repository.PassengerRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 @Service
 public class PassengerService {
     private final PassengerRepository repo;
     public PassengerService(PassengerRepository repo){ this.repo = repo; }
-    public List<Passenger> findAll(){ return repo.findAll(); }
+    public Page<Passenger> findAll(Pageable pageable){ return repo.findAll(pageable); }
     public Passenger findById(Long id){ return repo.findById(id).orElse(null); }
     public Passenger save(Passenger p){ return repo.save(p); }
     public void delete(Long id){ repo.findById(id).ifPresent(repo::delete); }
