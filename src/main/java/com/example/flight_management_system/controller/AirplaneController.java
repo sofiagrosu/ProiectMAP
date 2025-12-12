7f1092624d24aa3e95cf38c0b600a1b5ae886cfa
sofiagrosu <grosu.sofia@gmail.com>
2025-12-12 10:26:40 +0200

    Sort airplane

--- src/main/java/com/example/flight_management_system/controller/AirplaneController.java
+++ src/main/java/com/example/flight_management_system/controller/AirplaneController.java
@@ -1,9 +1,15 @@
 package com.example.flight_management_system.controller;
 
+import com.example.flight_management_system.model.AirlineEmployee;
 import com.example.flight_management_system.model.Airplane;
-import com.example.flight_management_system.repository.AirplaneRepository;
+
+import com.example.flight_management_system.service.AirplaneService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,13 +20,36 @@ import org.springframework.web.bind.annotation.*;
 public class AirplaneController {
 
     @Autowired
-    private AirplaneRepository airplaneRepository;
+    private AirplaneService airplaneService;
 
     @GetMapping
-    public String listAirplanes(Model model) {
-        model.addAttribute("airplanes", airplaneRepository.findAll());
-        return "airplanes/index";
-    }
+    public String listAirplanes(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "5") int size,
+        @RequestParam(defaultValue = "id") String sortBy,
+        @RequestParam(defaultValue = "true") boolean ascending,
+        Model model)
+        {
+
+            Sort sort = buildSort(sortBy, ascending);
+            Pageable pageable = PageRequest.of(page, size, sort);
+            Page<Airplane> objectPage = airplaneService.findAll(pageable);
+            model.addAttribute("airplanes", objectPage.getContent());     // pentru tabel
+            model.addAttribute("page", objectPage);                     // pentru paginare (opțional)
+            model.addAttribute("sortBy", sortBy);
+            model.addAttribute("ascending", ascending);
+
+            return "airplanes/index";
+
+
+        }
+        private Sort buildSort(String sortBy, boolean ascending) {
+            return switch (sortBy) {
+                case "id","number"-> ascending ? Sort.by(sortBy).ascending()
+                        : Sort.by(sortBy).descending();
+                default -> Sort.by("id").ascending();
+            };
+        }
 
     @GetMapping("/new")
     public String createAirplaneForm(Model model) {
@@ -34,37 +63,37 @@ public class AirplaneController {
 
         if (result.hasErrors()) return "airplanes/form";
 
-        Airplane existingAirplane = airplaneRepository.findByNumber(airplane.getNumber());
+        Airplane existingAirplane = airplaneService.findByNumber(airplane.getNumber());
 
         if (existingAirplane != null && !existingAirplane.getId().equals(airplane.getId())) {
             result.rejectValue("number", "unique", "An airplane with this number already exists.");
             return "airplanes/form";
         }
 
-        airplaneRepository.save(airplane);
+        airplaneService.save(airplane);
         return "redirect:/airplanes";
     }
 
     @GetMapping("/edit/{id}")
     public String editAirplaneForm(@PathVariable("id") Long id, Model model) {
-        Airplane airplane = airplaneRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid airplane Id:" + id));
+        Airplane airplane = airplaneService.findById(id);
+             //   .orElseThrow(() -> new IllegalArgumentException("Invalid airplane Id:" + id));
         model.addAttribute("airplane", airplane);
         return "airplanes/form";
     }
 
     @GetMapping("/delete/{id}")
     public String deleteAirplane(@PathVariable("id") Long id) {
-        Airplane airplane = airplaneRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid airplane Id:" + id));
-        airplaneRepository.delete(airplane);
+        Airplane airplane = airplaneService.findById(id);
+            //    .orElseThrow(() -> new IllegalArgumentException("Invalid airplane Id:" + id));
+        airplaneService.delete(airplane.getId());
         return "redirect:/airplanes";
     }
 
     @GetMapping("/details/{id}")
     public String airplaneDetails(@PathVariable("id") Long id, Model model) {
-        Airplane airplane = airplaneRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid airplane Id:" + id));
+        Airplane airplane = airplaneService.findById(id);
+           //     .orElseThrow(() -> new IllegalArgumentException("Invalid airplane Id:" + id));
         model.addAttribute("airplane", airplane);
         return "airplanes/details";
     }
--- src/main/java/com/example/flight_management_system/service/AirplaneService.java
+++ src/main/java/com/example/flight_management_system/service/AirplaneService.java
@@ -4,13 +4,18 @@ import com.example.flight_management_system.model.Airplane;
 import com.example.flight_management_system.repository.AirplaneRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 @Service
 public class AirplaneService {
     private final AirplaneRepository repo;
     public AirplaneService(AirplaneRepository repo){ this.repo = repo; }
-    public List<Airplane> findAll(){ return repo.findAll(); }
+    public Page<Airplane> findAll(Pageable pageable){ return repo.findAll(pageable); }
     public Airplane findById(Long id){ return repo.findById(id).orElse(null); }
     public Airplane save(Airplane a){ return repo.save(a); }
     public void delete(Long id){ repo.findById(id).ifPresent(repo::delete); }
+
+    public Airplane findByNumber(int number) {
+        return repo.findByNumber(number);
+    }
 }
